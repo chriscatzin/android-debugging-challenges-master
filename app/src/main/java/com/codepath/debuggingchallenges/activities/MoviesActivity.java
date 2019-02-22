2389fc844b37508a914c173f62f55ef3e3b7faae
Chris Catzin <ckc196@humboldt.edu>
2019-02-21 20:55:27 -0800

    Initial commit

---
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/MoviesActivity.java
@@ -0,0 +1,63 @@
+package com.codepath.debuggingchallenges.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import com.codepath.debuggingchallenges.R;
+import com.codepath.debuggingchallenges.adapters.MoviesAdapter;
+import com.codepath.debuggingchallenges.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+
+public class MoviesActivity extends AppCompatActivity {
+
+    private static final String API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    private static final String URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+    RecyclerView rvMovies;
+    MoviesAdapter adapter;
+    ArrayList<Movie> movies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+        rvMovies = findViewById(R.id.rvMovies);
+
+        movies = new ArrayList<>();
+        // Create the adapter to convert the array to views
+        MoviesAdapter adapter = new MoviesAdapter(movies);
+        // Attach the adapter to a ListView
+        rvMovies.setAdapter(adapter);
+
+        //fetchMovies();
+
+    AsyncHttpClient client = new AsyncHttpClient();
+        client.get(URL, null, new JsonHttpResponseHandler() {
+
+    //private void fetchMovies() {
+        //String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=";
+        //AsyncHttpClient client = new AsyncHttpClient();
+        //client.get(URL, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONArray moviesJson = response.getJSONArray("results");
+                    movies = Movie.fromJSONArray(moviesJson);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
